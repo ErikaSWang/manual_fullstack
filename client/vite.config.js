4aec9812768b5f3b57f4bc2c24dbd8f116605247
Erika Wang <ErikaWang@gmail.com>
2026-02-01 14:34:56 -0500

    Replit back to VScode still working (although the whitelisted repl urls have disappeared?)

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5000,
     host: '0.0.0.0',
-    allowedHosts: 'all',
+    port: 5000,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true
       }
-    }
+    },
+    allowedHosts: 'all'
   }
 })
